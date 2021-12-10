04ba2fa74d76d3af8b66e4874642fbb0f24bfb19
bill <bill@bigmojo.net>
2021-12-10 13:57:31 -0500

    added navigation to the buttons

--- lib/widgets/TaskColumn.dart
+++ lib/widgets/TaskColumn.dart
@@ -7,48 +7,56 @@ class TaskColumn extends StatelessWidget {
   final String subtitle;
 
   final double iconSize;
+
+  var onTapNav;
   TaskColumn({
     required this.icon,
     required this.iconBackgroundColor,
     required this.iconSize,
     required this.title,
     required this.subtitle,
+    required this.onTapNav,
   });
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        CircleAvatar(
-          radius: 20.0,
-          backgroundColor: iconBackgroundColor,
-          child: Icon(
-            icon,
-            size: iconSize,
-            color: Colors.white,
-          ),
-        ),
-        SizedBox(width: 10.0),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    return new GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, onTapNav);
+        },
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text(
-              title,
-              style: TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.w700,
+            CircleAvatar(
+              radius: 20.0,
+              backgroundColor: iconBackgroundColor,
+              child: Icon(
+                icon,
+                size: iconSize,
+                color: Colors.white,
               ),
             ),
-            Text(
-              subtitle,
-              style: TextStyle(
-                  fontSize: 14.0,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black45),
-            ),
+            SizedBox(width: 10.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  subtitle,
+                  style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black45),
+                ),
+              ],
+            )
           ],
-        )
-      ],
-    );
+        ));
   }
 }
